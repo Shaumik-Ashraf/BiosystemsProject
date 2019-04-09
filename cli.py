2352e23943a6cf584dd30915d6c220ee92d58d0f
Shaumik-Ashraf <a.shaumik@gmail.com>
2019-04-09 18:49:51 -0400

    added help text and info function

--- cli.py
+++ cli.py
@@ -6,33 +6,70 @@
 import kegg;
 
 help_text = """
-	Please give us an extension
+        This is a command-line interface program for interacting with
+        the KEGG database. It operates by entering lines in the following
+        format:
+                <command> <arguement 1> <arguement 2> ... <arguement n> <ENTER>
+
+        where < > is replaced by what the word within the brackets represent
+        (WITHOUT the brackets themselves.) The number of arguements required
+        for each command various. Here is a list of all commands, the arguements
+        required (if any), and what they do:
+
+                help (no arguements) - print this message
+                exit (no arguements) - end program
+                list <database> - lists all entries in the database specified (see below for possible databases)
+                find <database> <term to search> - returns all entries in database with term specified
+                get <kegg id> - returns information about object with specified kegg id
+                info <database> - returns information about the database
+
+        The databases in KEGG include but are not limited to:
+                reaction
+                enzyme
+                genes
+                compound
+                pathway
+
+        Note for get command:
+                possible KEGG IDs include R03544, 3.2.42.1, C00116, and map12345
+                the get command has only been programmed to check fo reaction,
+                enzyme, compound, and map databases.
+
+        See https://kegg.jp for more information on KEGG.
+                
+        Also, please give us an extension.
 """
 
 def needs_args(n):
-	if len(command) < n:
-		print("Warning, at least " + str(n-1) + " command arguement(s) required.");
+        if len(command) < n:
+                print("Warning, at least " + str(n-1) + " command arguement(s) required.");
 
 # ========command-line loop================================
-print( "Start" );
+print( "Start\n" );
 while 1==1:
-	command = input().strip().split(" ");
-	if command[0] == "help":
-		print(help_text)
-	elif command[0] == "exit":
-		break;
-	elif command[0] == "list":
-		needs_args(2);
-		out = kegg.list(command[1])
-		print( str(out) );
-	elif command[0] == "find":
-		needs_args(2);
-		out = kegg.find(command[1])
-		print( str(out) );
-	elif command[0] == "get":
-		needs_args(2);
-		out = kegg.get(command[1])
-		print( str(out) );
-	else:
-		print("Command unrecognized");
+        print("kegg-cli>>", end="");
+        command = input().strip().split(" ");
+        if command[0] == "help":
+                print(help_text)
+        elif command[0] == "exit":
+                break;
+        elif command[0] == "list":
+                needs_args(2);
+                out = kegg.list(command[1])
+                print( str(out) );
+        elif command[0] == "find":
+                needs_args(3);
+                out = kegg.find( "{0}/{1}".format(command[1],command[2]) );
+                print( str(out) );
+        elif command[0] == "get":
+                needs_args(2);
+                out = kegg.get(command[1])
+                print( str(out) );
+        elif command[0] == "info":
+                needs_args(2);
+                out = kegg.info(command[1])
+                print( str(out) );
+        else:
+                print("Command unrecognized");
 
+print( "End" )
--- kegg.py
+++ kegg.py
@@ -7,44 +7,50 @@ import urllib3;
 http = urllib3.PoolManager();
 
 def list(datatype):
-	newstr = str(http.request('GET', 'http://rest.kegg.jp/list/{datatype}/'.format(datatype=datatype)).data)
-	newstr = newstr.replace("\\t", "\t");
-	newlist = newstr.split('\\n');
-	newlist[0] = newlist[0][2:]; #shave off trash at beggining of first element
-	del newlist[len(newlist)-1]; #shave off last element (trash)
-	return(newlist);
+        newstr = str(http.request('GET', 'http://rest.kegg.jp/list/{datatype}/'.format(datatype=datatype)).data)
+        newstr = newstr.replace("\\t", "\t");
+        newlist = newstr.split('\\n');
+        newlist[0] = newlist[0][2:]; #shave off trash at beggining of first element
+        del newlist[len(newlist)-1]; #shave off last element (trash)
+        return(newlist);
     
 def find(datatype):
-	newstr = str(http.request('GET', 'http://rest.kegg.jp/find/{datatype}/'.format(datatype=datatype)).data)
-	newstr = newstr.replace("\\t", "\t");
-	newlist = newstr.split('\\n');
-	newlist[0] = newlist[0][2:]; #shave off trash at beggining of first element
-	del newlist[len(newlist)-1]; #shave off last element (trash)
-	return(newlist);
+        newstr = str(http.request('GET', 'http://rest.kegg.jp/find/{datatype}/'.format(datatype=datatype)).data)
+        newstr = newstr.replace("\\t", "\t");
+        newlist = newstr.split('\\n');
+        newlist[0] = newlist[0][2:]; #shave off trash at beggining of first element
+        del newlist[len(newlist)-1]; #shave off last element (trash)
+        return(newlist);
 
 def get(query):
-	if query[0] == "R":
-		prefix = "rn"
-	elif query[0] == "C":
-		prefix = "cpd"
-	elif query[0:3] == "map":
-		prefix = "path"
-	elif len(query.split('.')) == 4:
-		prefix = "ec"
-	else:
-		print("Warning: query type unindentified");
-		
-	#print("DEBUG: ", "query=", query);
-	#print("DEBUG: ", "req=", 'http://rest.kegg.jp/get/{0}:{1}'.format(prefix,query));
-	text=str(http.request('GET', 'http://rest.kegg.jp/get/{0}:{1}'.format(prefix,query)).data);
-	#print("DEBUG: ", "text=", text);
-	text = text.replace("\\t", "\t");
-	text = text.replace("\\n", "\n");
-	text = text[2:len(text)-1]; #rm first 2 and last characters (trash)
-	return(text)
+        if query[0] == "R":
+                prefix = "rn"
+        elif query[0] == "C":
+                prefix = "cpd"
+        elif query[0:3] == "map":
+                prefix = "path"
+        elif len(query.split('.')) == 4:
+                prefix = "ec"
+        else:
+                print("Warning: query type unindentified");
+                
+        text=str(http.request('GET', 'http://rest.kegg.jp/get/{0}:{1}'.format(prefix,query)).data);
+        text = text.replace("\\t", "\t");
+        text = text.replace("\\n", "\n");
+        text = text[2:len(text)-1]; #rm first 2 and last characters (trash)
+        return(text)
 
 #get command id format to prefix map
 #reaction: <id> = Rxxxxx, <prefix> = rn
 #enzyme: <id> = n.n.n.n, <prefix> = ec
 #path: <id> = mapxxxxx, <prefix> = path
 #compound: <id> = Cxxxxx, <prefix> = cpd
+
+def info(database):
+        text=str(http.request('GET', 'http://rest.kegg.jp/info/{0}'.format(database)).data);
+        #print("DEBUG: ", "text=", text);
+        text = text.replace("\\t", "\t");
+        text = text.replace("\\n", "\n");
+        text = text[2:len(text)-1]; #rm first 2 and last characters (trash)
+        return(text)
+
