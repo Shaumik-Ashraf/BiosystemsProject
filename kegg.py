1e2bb7ea5a3f84ee211bca07597656cd7509a549
Shaumik Ashraf <a.shaumik@gmail.com>
2019-04-24 18:19:32 -0400

    did stuff

--- kegg.py
+++ kegg.py
@@ -54,3 +54,50 @@ def info(database):
         text = text[2:len(text)-1]; #rm first 2 and last characters (trash)
         return(text)
 
+#direct simply joins the array with /s and sends request to kegg
+def direct(arg_list):
+        request = "http://rest.kegg.jp/"
+        for s in arg_list:
+                request += "/".join(arg_list);
+        text=str(http.request('GET', 'http://rest.kegg.jp/info/{0}'.format(database)).data);
+
+
+
+ignore_this_string = """
+def A2B(compoundA, compoundB):
+        #set variable idA, ask if compoundA is correctly found
+        templist = find( 'compound/{0}'.format(compoundA) );
+        found = False;
+        for i in range(len(templist)):
+                templist2 = templist[i].split('\t')
+                idA = templist2[0]
+                print( 'Is your first compound also known as: {0}? [y/n]'.format(templist2[1]) )
+                in_buffer = input();
+                if in_buffer.upper().startswith('Y'):
+                        found = True;
+                        break;
+                else:
+                        continue;
+        if( !found ):
+                print( '{0} could not be found.'.format(compoundA.capitalize()) )
+                return( -1 );
+        
+        #set variable idB, ask if compoundB is correctly found
+        templist = find( 'compound/{0}'.format(compoundB) );
+        found = False;
+        for i in range(len(templist)):
+                templist2 = templist[i].split('\t')
+                idB = templist2[0]
+                print( 'Is your last compound also known as: {0}? [y/n]'.format(templist2[1]) )
+                in_buffer = input();
+                if in_buffer.upper().startswith('Y'):
+                        found = True;
+                        break;
+                else:
+                        continue;
+        if( !found ):
+                print( '{0} could not be found.'.format( compoundB.capitalize() ) )
+                return( -1 );
+        
+        #To be continued....
+"""
